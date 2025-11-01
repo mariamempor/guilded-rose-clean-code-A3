9be647d69f19eb3f35c91dd908ecdde6de653aee
Maria <mariaeduarda.portom@gmail.com>
2025-11-01 15:02:04 -0300

    refactor: extração de métodos para tipos de itens (commit 2)

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -12,38 +12,50 @@ public class GildedRose {
     public void updateQuality() {
         for (Item item : items) {
             if (item.name.equals(ItemNames.AGED_BRIE)) {
-                if (item.quality < 50) {
-                    item.quality++;
-                }
-                item.sellIn--;
-                if (item.sellIn < 0 && item.quality < 50) {
-                    item.quality++;
-                }
+                updateAgedBrie(item);
             } else if (item.name.equals(ItemNames.BACKSTAGE_PASSES)) {
-                if (item.quality < 50) {
-                    item.quality++;
-                    if (item.sellIn < 11 && item.quality < 50) {
-                        item.quality++;
-                    }
-                    if (item.sellIn < 6 && item.quality < 50) {
-                        item.quality++;
-                    }
-                }
-                item.sellIn--;
-                if (item.sellIn < 0) {
-                    item.quality = 0;
-                }
+                updateBackstagePasses(item);
             } else if (item.name.equals(ItemNames.SULFURAS)) {
                 // Sulfuras não muda
             } else {
-                if (item.quality > 0) {
-                    item.quality--;
-                }
-                item.sellIn--;
-                if (item.sellIn < 0 && item.quality > 0) {
-                    item.quality--;
-                }
+                updateNormalItem(item);
             }
+        }
+    }
+
+    private void updateNormalItem(Item item) {
+        if (item.quality > 0) {
+            item.quality--;
+        }
+        item.sellIn--;
+        if (item.sellIn < 0 && item.quality > 0) {
+            item.quality--;
+        }
+    }
+
+    private void updateAgedBrie(Item item) {
+        if (item.quality < 50) {
+            item.quality++;
+        }
+        item.sellIn--;
+        if (item.sellIn < 0 && item.quality < 50) {
+            item.quality++;
+        }
+    }
+
+    private void updateBackstagePasses(Item item) {
+        if (item.quality < 50) {
+            item.quality++;
+            if (item.sellIn < 11 && item.quality < 50) {
+                item.quality++;
+            }
+            if (item.sellIn < 6 && item.quality < 50) {
+                item.quality++;
+            }
+        }
+        item.sellIn--;
+        if (item.sellIn < 0) {
+            item.quality = 0;
         }
     }
 }
