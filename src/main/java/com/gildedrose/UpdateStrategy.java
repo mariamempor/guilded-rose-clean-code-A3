f99a36f2234c618151b91bf80a36cfe574d79890
Maria <157513871+mariamempor@users.noreply.github.com>
2025-11-10 10:51:34 -0300

    Update UpdateStrategy.java
    Correção erros testes unitários

--- src/main/java/com/gildedrose/UpdateStrategy.java
+++ src/main/java/com/gildedrose/UpdateStrategy.java
@@ -1,9 +1,5 @@
 package com.gildedrose;
 
 public interface UpdateStrategy {
-    default void update(Item item) {
-        // Validação comum: qualidade entre 0 e 50 (exceto Sulfuras)
-        if (item.quality < 0) item.quality = 0;
-        if (item.quality > 50 && !item.name.equals(ItemNames.SULFURAS)) item.quality = 50;
-    }
+    void update(Item item);
 }
